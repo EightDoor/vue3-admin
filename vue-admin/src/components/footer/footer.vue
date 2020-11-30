ce2d02fd5633e7b4ba120fc569d21d8f8364fec1
eightdoor <851708184@qq.com>
2020-11-30 18:26:29 +0800

    fix

---
+++ vue-admin/src/components/footer/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  1
+</template>
+<script lang="ts">
+import {defineComponent} from 'vue';
+export default defineComponent({
+  setup() {
+    return {
+
+    }
+  }
+})
+</script>
+<style scoped lang="less">
+@import "footer.less";
+</style>
